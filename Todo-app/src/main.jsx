64e4d2de49855fe49ba2ab69b587456664964578
Prakhar Singh <96379148+Shrike0p@users.noreply.github.com>
2024-07-05 03:39:53 +0530

    Add files via upload

---
+++ Todo-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App2 from './App2'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App2 />
+  </React.StrictMode>,
+)
