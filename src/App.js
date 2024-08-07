51c3bc020909544c4094c814c841ca4bbb9ac0b7
Prakhar Singh <prakharsingh2208@gmail.com>
2024-08-07 18:40:45 +0530

    Revert "Merge remote-tracking branch 'origin/master' into master"
    This reverts commit 0d758fa377022693053d9d3e1299d087ac520418.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import MainPage from './Component/MainPage';
-import Mealinfo from './Component/Mealinfo';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/:mealid' element={<Mealinfo />} />
-      </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
